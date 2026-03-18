c6f6f2019162a50514f62d9501de1ed945bc6599
Max <yzh5277@outlook.com>
2026-03-18 21:18:52 +0800

    Initial Commit

---
+++ PeakLog/PeakLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeakLogApp.swift
+//  PeakLog
+//
+//  Created by max on 2026/3/18.
+//
+
+import SwiftUI
+
+@main
+struct PeakLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
